18aaceda785a0a504f407101e85d51be402d34e4
anoochit <anoochit@ca463cd0-b787-11dd-8af8-77a15857c330>
2008-12-18 04:05:11 +0000

    add config.inc.php for config the front page of your repository

--- trunk/index.php
+++ trunk/index.php
@@ -1,13 +1,17 @@
+<?
+
+include_once("config.inc.php");
+
+?>
 <html>
 <title>Ubuntu Repository</title>
 <body>
-<img src="ubuntuclub-logo.png">
-<p>This is a simple ubuntu repository for ubuntu club package, please update your source.list to use it.</p>
-<em><p>sudo gedit /etc/apt/sources.list<br>
-deb http://thaiopensource.org/ubuntu intrepid main</p></em>
-<p>or download and install <a href="/ubuntu/pool/main/u/ubuntuclub-repository_0.1ubuntu1-1_i386.deb">ubuntuclub-repository</a> package.</p>
+<img src="<?=$logo;?>">
+<p>This is a simple ubuntu repository for ubuntu package, please update your source.list to use it.</p>
+<em><p>sudo gedit /etc/apt/sources.list.d/ubuntuclub.list<br>
+deb <?=$apturl; ?> <?=$dist;?> <?=$repo; ?></p></em>
 <p>Thank you</p>
-* You can view package list at <a href="http://www.thaiopensource.org/ubuntu/packagelist.txt">packagelist.txt</a>.
+* You can view package list at <a href="<?=$apturl; ?>/packagelist.txt">packagelist.txt</a>.
 <br><br>
 <table bgcolor="#BBBBBB" cellpadding="3" cellspacing="1" width="600">
 <tr bgcolor="#DDDDDD">
